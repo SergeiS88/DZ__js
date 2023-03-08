abbf1e3126f8c2abc0f288ac29bd1edf2248a84f
SergeiS88 <svsubbotin88@mail.ru>
2023-03-08 13:46:57 +0300

    ДЗ_8

---
+++ 7script.js
@@ -0,0 +1,35 @@
+    "use strict";
+// []()
+// []()
+// 1. Ко всем элементам, имеющим класс "dropdown-item" добавить еще один класс "super-dropdown", необходимо использовать методы forEach и querySelectorAll и свойство classList у элементов.
+    document.querySelectorAll('.dropdown-item').forEach(el => el.classList.add('super-dropdown'))
+
+// []()
+// 2. У элемента с классом btn необходимо убрать класс "btn-secondary", если он присутствует у этого элемента, либо добавить, если такого класса у элемента не было.
+    document.querySelector('.btn').classList.forEach(el => {
+        el == 'btn-secondary'?
+            document.querySelector('.btn').classList.remove('btn-secondary'):
+            document.querySelector('.btn').classList.add('btn-secondary');
+    })
+
+// []()
+// 3. Необходимо удалить класс "dropdown-menu" у элемента, у которого присутствует класс "menu".
+    document.querySelectorAll('.menu').forEach(el => el.classList.remove('dropdown-menu'));
+
+// []()
+// 4. Используя метод insertAdjacentHTML добавьте после div'a с классом "dropdown" следующую разметку:
+// `<a href="#">link</a>`
+        document.querySelector('div.dropdown').insertAdjacentHTML("afterend", '<a href="#">link</a>')
+
+// []()
+// 5. У элемента с id "dropdownMenuButton" замените id на "superDropdown".
+        document.querySelector('#dropdownMenuButton').id = 'superDropdown';
+
+// []()
+// 6. Добавьте атрибут data-dd со значением 3 элементу у которого существует атрибут "aria-labelledby" равный "dropdownMenuButton" используя dataset.
+        document.querySelectorAll('[aria-labelledby="dropdownMenuButton"]').forEach(el => el.dataset.dd = '3');
+
+// []()
+// 7. Удалите атрибут type у элемента с классом "dropdown-toggle".
+        document.querySelectorAll('.dropdown-toggle').forEach(el => el.removeAttribute('type'))
+
